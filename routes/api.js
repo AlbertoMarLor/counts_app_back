5dc9858a20fd8f49e8492b27ec0e37a4d5930aaa
sarafbrun <sarafuen.93@gmail.com>
2023-04-14 10:31:32 +0200

    prueba peticiones.rest users ok

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const { checkToken } = require('../helpers/middlewares');
 const router = require('express').Router();
 
 
-router.use('/groups', require('./api/group'));
+router.use('/groups', checkToken, require('./api/group'));
 router.use('/users', require('./api/users'));
 
 
