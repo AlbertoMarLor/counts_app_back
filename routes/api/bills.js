18e03223c1201541b1cdf0c316f48c786d0e0158
sarafbrun <sarafuen.93@gmail.com>
2023-04-25 00:39:11 +0200

    cambios en el bill-model y bills create

--- routes/api/bills.js
+++ routes/api/bills.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 
-const { getById, deleteBill, getAll, create, updateById, createGroupsHasBills, getUsersHasBills, creditorUsersHasBills, getTotalAmount, findBillByName, operations, getOperations } = require('../../models/bills.model');
+const { getById, deleteBill, getAll, create, updateById, createGroupsHasBills, getUsersHasBills, creditorUsersHasBills, getTotalAmount, findBillByName, operations, getOperations, updateOperations, quantityOperations } = require('../../models/bills.model');
+const { getUserById } = require('../../models/users.model')
 const { getUsersFromGroup, countGroupMembers } = require('../../models/groups.model');
 const { checkAdmin } = require('../../helpers/middlewares');
 
@@ -56,7 +57,7 @@ router.get('/totalAmount/:groupId', async (req, res) => {
     try {
         const [totalAmount] = await getTotalAmount(groupId);
         res.json(totalAmount);
-        console.log(totalAmount);
+
     } catch (error) {
         res.json({ fatal: error.message });
     }
@@ -65,7 +66,7 @@ router.get('/totalAmount/:groupId', async (req, res) => {
 router.get('/amount/debts/:groupId/users', checkAdmin(), async (req, res) => {
     const { groupId } = req.params;
     try {
-        const [totalAmount] = await getTotalAmount(groupId);
+        /* const [totalAmount] = await getTotalAmount(groupId);
         const [members] = await getUsersFromGroup(groupId)
         const [nMembers] = await countGroupMembers(groupId);
         const admin = req.admin[0].role;
@@ -81,16 +82,23 @@ router.get('/amount/debts/:groupId/users', checkAdmin(), async (req, res) => {
 
 
         for (let member of members) {
-            console.log(member)
-            if (member.role === admin) {
-                const res = await operations(member.id, member.username, totalDept, groupId)
-                console.log(res)
 
-            } else {
-                const res2 = await operations(member.id, member.username, memberDebt, groupId)
-                console.log(res2)
+            const user = getById(member.id)
+            if (user.id !== member.id) {
+
+                //console.log(member)
+                if (member.role === admin) {
+                    const res = await operations(member.id, member.username, totalDept, groupId)
+                    //console.log(res)
+
+                } else {
+                    const res2 = await operations(member.id, member.username, memberDebt, groupId)
+                    //console.log(res2)
+                }
             }
-        }
+        } */
+
+
 
         const [finalOperations] = await getOperations(groupId)
         return res.json(finalOperations)
@@ -121,19 +129,75 @@ router.post('/:groupId/newBill', checkAdmin(), async (req, res) => {
         const [result] = await create(req.body);
         const [newBill] = await getById(result.insertId);
         const [membersGroup] = await getUsersFromGroup(groupId);
+        const addBill = (Number(newBill[0].quantity));
+        console.log(addBill)
 
         await createGroupsHasBills(groupId, newBill[0].id)
         for (let member of membersGroup) {
             if (member.role === admin) {
-                await creditorUsersHasBills(member.id, newBill[0].id, 1, newBill[0].quantity)
+                await creditorUsersHasBills(member.id, newBill[0].id, 1, addBill)
 
 
             } else {
-                await creditorUsersHasBills(member.id, newBill[0].id, 0, newBill[0].quantity)
+                await creditorUsersHasBills(member.id, newBill[0].id, 0, addBill)
             }
-
-
         }
+
+        //Inserción tabla intermedia operations
+        let [totalAmount] = await getTotalAmount(groupId);
+        const [members] = await getUsersFromGroup(groupId)
+        const [nMembers] = await countGroupMembers(groupId);
+
+
+        let { suma } = totalAmount[0];
+        let { totalMembers } = nMembers[0]
+
+        //Lo que debe cada participante del grupo
+        let memberDebt = suma / totalMembers;
+        //Lo que tiene que cobrar el acreedor (admin del grupo)
+        let totalDept = suma - memberDebt;
+
+
+        for (let member of members) {
+            let [user] = await getUserById(member.id)
+            //console.log(user[0].id, member.id)
+            if (user[0].id !== member.id) {
+
+                if (member.role === admin) {
+                    await operations(member.id, member.username, totalDept, groupId)
+                    //console.log(res)
+
+                } else {
+                    await operations(member.id, member.username, memberDebt, groupId)
+                    //console.log(res2)
+                }
+            } else {
+                //let [totalAmount] = await getTotalAmount(groupId);
+                let [inicialQuantity] = await quantityOperations(member.id)
+
+                let inicial = (Number(inicialQuantity[0].quantity))
+                console.log(inicial)
+
+                const divisionBill = addBill / totalMembers
+
+                const deptDeptors = divisionBill * (totalMembers - 1)
+
+                //Lo que tiene que cobrar el acreedor (admin del grupo)
+                let totalDept = inicial - divisionBill + deptDeptors;
+                //console.log(inicial, '-', divisionBill, '+', deptDeptors, '=', totalDept)
+                if (member.role === admin) {
+                    await updateOperations(totalDept)
+
+
+
+                } else {
+                    await updateOperations(divisionBill)
+                    console.log(divisionBill)
+                }
+
+            }
+        }
+
 
 
         res.json(newBill[0]);
